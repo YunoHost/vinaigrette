bee4cabad20da3530f7e61f1ed0858221a715da8
Alexandre Aubin <alexandre.aubin@cern.ch>
2017-08-30 14:03:09 +0200

    Moar fixes

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 
-apt-get install nginx pbuilder reprepro rebuildd gawk -y
+apt-get install nginx pbuilder reprepro rebuildd gawk sendxmpp -y
 
 VINAIGRETTE_HOME="/home/vinaigrette"
 
@@ -16,21 +16,19 @@ git clone https://github.com/yunohost/ssowat
 git clone https://github.com/yunohost/moulinette
 
 mkdir -p /var/www/repo/debian/conf/
+ln -s $VINAIGRETTE_HOME/config/distributions /var/www/repo/debian/conf/distributions
 
-cd $VINAIGRETTE_HOME/
-cp config/distributions /var/www/repo/debian/conf/
-
-cp $VINAIGRETTE_HOME/config/rebuildd.conf /etc/rebuildd/rebuilddrc
-
-cat $VINAIGRETTE_HOME/config/keys/$DEBSIGN_KEYID.pub | apt-key add
-cat $VINAIGRETTE_HOME/config/sources.list >> /etc/apt/sources.list
+rm /etc/rebuildd/rebuilddrc
+ln -s $VINAIGRETTE_HOME/config/rebuildd.conf /etc/rebuildd/rebuilddrc
 
 ln -s $VINAIGRETTE_HOME/pbuilder/images /var/cache/pbuilder/images
-ln -s /var/cache/pbuilder/result $VINAIGRETTE_HOME/pbuilder/result
+ln -s /var/cache/pbuilder/result $PBUILDER_RESULTS
+cp $VINAIGRETTE_HOME/conf/nginx.conf /etc/nginx/sites-enabled/repo.conf
 
+cat $VINAIGRETTE_HOME/config/keys/$DEBSIGN_KEYID.pub | apt-key add
+cat $VINAIGRETTE_HOME/config/sources.list > /etc/apt/sources.list.d/vinaigrette.list
 
 echo "127.0.0.1 $REPO_URL" >> /etc/hosts
-cp $VINAIGRETTE_HOME/conf/nginx.conf /etc/nginx/sites-enabled/repo.conf
 service nginx reload
 
 rebuildd init
